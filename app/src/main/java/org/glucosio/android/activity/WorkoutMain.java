11d0ed60487c7dbc5ec7cd0bbeb21875e29a555b
Julie Huang <huangj11@tcnj.edu>
2018-04-09 16:20:03 -0400

    Prototype
    Added Workout Mode prototype

---
+++ app/src/main/java/org/glucosio/android/activity/WorkoutMain.java
@@ -0,0 +1,14 @@
+package org.glucosio.android.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import org.glucosio.android.R;
+
+public class WorkoutMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_workout_main);
+    }
+}
